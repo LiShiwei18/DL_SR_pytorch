44da2904abe828c511b995f247eba319db64b4d3
regin-3_a5000 <484850093@qq.com>
2023-05-04 15:49:43 +0800

    在loss里面写了测试代码，结果差一点点。不知道是可以接受的范围还是因为是显卡不是2080ti的关系

--- src/utils/torch_loss.py
+++ src/utils/torch_loss.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn.functional as F
+from ssim import SSIM
 
 
 def loss_mse_ssim(y_true, y_pred):
@@ -11,8 +12,8 @@ def loss_mse_ssim(y_true, y_pred):
     y = y_pred
     x = (x - torch.min(x)) / (torch.max(x) - torch.min(x))
     y = (y - torch.min(y)) / (torch.max(y) - torch.min(y))
-
-    ssim_loss = ssim_para * (1 - F.ssim(x, y, data_range=1, size_average=True))
+    ssim = SSIM()
+    ssim_loss = ssim_para * (1 - ssim(x, y))
     mse_loss = mse_para * F.mse_loss(y, x)
 
     return mse_loss + ssim_loss
@@ -32,3 +33,14 @@ def loss_mae_mse(y_true, y_pred):
     mse_loss = mse_para * F.mse_loss(y, x)
 
     return mae_loss + mse_loss
+
+if __name__ == "__main__":
+    # Generate random tensors with torch.seed()
+    torch.manual_seed(42)
+    y_true = torch.rand(5, 3, 256, 256)
+    y_pred = torch.rand(5, 3, 256, 256)
+    # Call the loss_mse_ssim function with the random tensors
+    loss = loss_mse_ssim(y_true, y_pred)
+    print(loss)
+
+    # result : 0.2644
