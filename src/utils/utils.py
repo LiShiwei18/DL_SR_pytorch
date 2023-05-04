8ea2aeaaa938deee92ea2b77ba682998f5b9962c
regin-3_a5000 <484850093@qq.com>
2023-05-04 15:53:05 +0800

    补充了img_comp函数

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -6,6 +6,31 @@ def prctile_norm(x, min_prc=0, max_prc=100):
     y[y < 0] = 0
     return y
 
+def img_comp(gt, pr, mses=None, nrmses=None, psnrs=None, ssims=None):
+    if ssims is None:
+        ssims = []
+    if psnrs is None:
+        psnrs = []
+    if nrmses is None:
+        nrmses = []
+    if mses is None:
+        mses = []
+    gt, pr = np.squeeze(gt), np.squeeze(pr)
+    gt = gt.astype(np.float32)
+    if gt.ndim == 2:
+        n = 1
+        gt = np.reshape(gt, (1, gt.shape[0], gt.shape[1]))
+        pr = np.reshape(pr, (1, pr.shape[0], pr.shape[1]))
+    else:
+        n = np.size(gt, 0)
+
+    for i in range(n):
+        mses.append(compare_mse(prctile_norm(np.squeeze(gt[i])), prctile_norm(np.squeeze(pr[i]))))
+        nrmses.append(compare_nrmse(prctile_norm(np.squeeze(gt[i])), prctile_norm(np.squeeze(pr[i]))))
+        psnrs.append(compare_psnr(prctile_norm(np.squeeze(gt[i])), prctile_norm(np.squeeze(pr[i])), 1))
+        ssims.append(compare_ssim(prctile_norm(np.squeeze(gt[i])), prctile_norm(np.squeeze(pr[i]))))
+    return mses, nrmses, psnrs, 
+
 def _diffxy(img, order=3):
     for _ in range(order):
         img = prctile_norm(img)
