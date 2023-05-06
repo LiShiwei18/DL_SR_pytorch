d590dd0015733155b94ff4953a3e160d63c6ea97
regin-3_a5000 <484850093@qq.com>
2023-05-06 19:33:06 +0800

    分母的小数改成1e-9,另外一段时间后lr真的会下降

--- src/utils/torch_loss.py
+++ src/utils/torch_loss.py
@@ -10,8 +10,8 @@ def loss_mse_ssim(y_true, y_pred):
     # normalization
     x = y_true
     y = y_pred
-    norm_x = (x - torch.min(x)) / (torch.max(x) - torch.min(x) + 1e-6)
-    norm_y = (y - torch.min(y)) / (torch.max(y) - torch.min(y) + 1e-6)
+    norm_x = (x - torch.min(x)) / (torch.max(x) - torch.min(x) + 1e-9)
+    norm_y = (y - torch.min(y)) / (torch.max(y) - torch.min(y) + 1e-9)
     ssim = SSIM()
     ssim_loss = ssim_para * (1 - ssim(norm_x, norm_y))
     mse_loss = mse_para * F.mse_loss(norm_y, norm_x)
